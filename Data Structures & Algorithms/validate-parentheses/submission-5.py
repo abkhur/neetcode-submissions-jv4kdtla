fc60a81cf5385beeafd3977bd083cef3eedf569f
abkhur <43392614+abkhur@users.noreply.github.com>
2026-04-19 00:41:24 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+      stack = []
+      meow = { ")": "(", "]": "[", "}": "{" }
+
+      for c in s:
+        if c in meow:
+            if stack and stack[-1] == meow[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+      return not stack
